dc72a5262f7039d36a7f9881ffb4e0070015681f
binyoung_im <qlsdud960604@gmail.com>
2022-09-18 16:38:01 +0900

    Implement #3 - DispatcherServlet 클래스
    web server 배포를 위한 DispatcherServlet 클래스 생성

--- src/main/java/com/fastcampus/spring/config/Config.java
+++ src/main/java/com/fastcampus/spring/config/Config.java
@@ -2,7 +2,9 @@ package com.fastcampus.spring.config;
 
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
+@EnableWebMvc
 @ComponentScan("com.fastcampus.spring")
 @Configuration
 public class Config {
